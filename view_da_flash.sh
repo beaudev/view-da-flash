b774975d87f10a9e83c1cf7fe4378308144c5a40
jmeyo <jc@houseofagile.com>
2014-08-16 12:39:40 +0200

    Find videos from Firefox instances also

--- view_da_flash.sh
+++ view_da_flash.sh
@@ -1,10 +1,16 @@
-#!/bin/bash 
+#!/bin/bash -x
  
 # That function could be added to your bash_profile for example
+ # send all the current videos loaded in your chromium browser or in Firefox into a vlc instance byt calling at any time "view_daflash"
  
 view_daflash(){
-    var=`ps -ef | grep -i flash | grep "chromium-browser --type=plugin"|nawk '{print $2}'`
-    for fd in `find /proc/$var/fd -type l`;do ls -l $fd|grep -i flash &>/dev/null &&echo $fd;done| xargs vlc
+    # Chromium with flashplugin
+    flash_procs=`ps -ef | grep -i flash | grep "chromium-browser --type=plugin"|nawk '{print $2}'`
+    # Firefox with flashplugin
+    flash_procs="${flash_procs[@]} `ps -ef | grep -i flash | grep "firefox"|nawk '{print $2}'`"
+    flash_vid=""
+	for proc in ${flash_procs[@]}; do 
+		for fd in `find /proc/$proc/fd -type l`;do ls -l $fd|grep -i flash &>/dev/null && flash_vid="${flash_vid[@]} $fd" ;done
+	done
+	[ ! -z "$flash_vid" ] && vlc $flash_vid || echo "Sorry No Flash Videos Found.."
 }
- 
-# send all the current videos loaded in your chromium browser into a vlc instance byt calling at any time "view_daflash"
