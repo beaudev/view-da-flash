7dfc78416adfd8c4a0000ad23f961e2d66ce6dda
jmeyo <jc@houseofagile.com>
2014-08-16 14:33:48 +0200

    Add saving option and better handling of files

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-view-da-flash
+View Da Flash
 =============
 
-Simple script to view embedded flash in a better player
+Simple script to view embedded flash in a better player (talking here about VLC of course). THe great thing about that, is you don't fight anymore with the auto lock functionnality, you can benefit of the VLC user friendly interface, even if the video is not fully loaded. Also, it's possible to save it quite easily. 
+
+Work on *Nix, mainly used on Ubuntu tho.
+Works with Firefox and Chromium as long as you use the Adobe flash plugin.
+
+## Install
+source the file, or use it with the bash-profile project.
+
+
+###OneLiner
+```
+curl -SsL https://raw.githubusercontent.com/beaudev/view-da-flash/master/view_da_flash.sh>source
+
+# view currently loaded flash videos :
+view_da_flash
+
+# save currently loaded flash videos in Downloads directory (by default)
+view_da_flash -s 
+```
+
+## Next steps
+* Add some more options for saving videos
+* make it work with chrome and latest chromium (under 14.04, chromium seems to use flash plugin in a different way)
+* your need, feel free to ask, we never know =] 
--- view_da_flash.sh
+++ view_da_flash.sh
@@ -4,13 +4,44 @@
  # send all the current videos loaded in your chromium browser or in Firefox into a vlc instance byt calling at any time "view_daflash"
  
 view_daflash(){
+	com_func_file=$( dirname "${BASH_SOURCE[0]}" )/common_functions.sh
+	if [ ! -f ${com_func_file} ]; then
+		curl -sSL https://raw.github.com/jmeyo/CommonBashScripts/master/common_functions.sh >${com_func_file}	
+	fi
+	source ${com_func_file}
+	
+	while getopts "s" optname
+	do
+		case "$optname" in
+		  "s")
+			SAVE_IT=true
+			;;         
+		  *)
+			cecho -e "\n\t-> $OPTARG Bad options\n\n" $red
+			help
+			;;
+		esac
+	done
+	[ ! -z "$1" ] && saveit=true
     # Chromium with flashplugin
     flash_procs=`ps -ef | grep -i flash | grep "chromium-browser --type=plugin"|nawk '{print $2}'`
     # Firefox with flashplugin
     flash_procs="${flash_procs[@]} `ps -ef | grep -i flash | grep "firefox"|nawk '{print $2}'`"
-    flash_vid=""
+    flash_vids=""
 	for proc in ${flash_procs[@]}; do 
-		for fd in `find /proc/$proc/fd -type l`;do ls -l $fd|grep -i flash &>/dev/null && flash_vid="${flash_vid[@]} $fd" ;done
+		for fd in `find /proc/$proc/fd -type l`;do ls -l $fd|grep -i flash &>/dev/null && flash_vids="${flash_vids[@]} $fd" ;done
 	done
-	[ ! -z "$flash_vid" ] && vlc $flash_vid || echo "Sorry No Flash Videos Found.."
+	if [ ! -z "$flash_vids" ]; then
+		if [ "$SAVE_IT" == "true" ];then
+			for flash_vid in ${flash_vids[@]}; do
+				file_name=$(echo ${flash_vid}| sed 's@.*/@@')
+				file_name_path=$(eval echo "${save_path:-"~/Downloads/"}${file_name}")
+				[ -f "${file_name_path}.avi" ] && file_name_path=$(echo ${file_name_path}"_"${RANDOM}".avi")
+				cp $flash_vid $file_name_path
+				cecho "Video saved under \"$file_name_path\"" $green
+			done
+		else
+			vlc $flash_vids || cecho "Sorry. No Flash Video Found.." $red
+		fi
+	fi 
 }
